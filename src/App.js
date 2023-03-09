e125b6397e8a6e81c7463bf57912118287ca6060
CTminhduc <102164853+CTminhduc@users.noreply.github.com>
2023-03-09 22:28:20 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import {useState} from 'react'
 
 function App() {
+  const [work, setWork] = useState('Moi Nhap')
+  console.log(work)
   return (
-    <div className="App">
-      app
+    <div className="h-screen gap-8 flex items-center border border-red-500 justify-center">
+      <input 
+      type="text"
+      className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+      value={work}
+      onChange={e => setWork(e.target.value)}
+      />
+      <button
+      type="button"
+      className="outline-none px-4 py-2 bg-blue-500 rounded-sm text-white"
+      >
+      Add
+      </button>
     </div>
   );
 }
